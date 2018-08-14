93daa3fa243aeb05a3bef812b3871d8011b937f2
adamaeroup <42358291+adamaeroup@users.noreply.github.com>
2018-08-14 11:27:59 -0500

    static (in front of object) but it may be void

--- REBLDisplay.h
+++ REBLDisplay.h
@@ -7,10 +7,14 @@
 #include "Defines.h"
 
 enum color_vars {BLACK, RED , GREEN , YELLOW , BLUE , PURPLE , CYAN , WHITE};
+#define LCD_ENABLE_PIN A1
+#define LCD_RS_PIN A3
+#define LCD_D1 4    // New ↓
+#define LCD_D2 5
+#define LCD_D3 6
+#define LCD_D4 7
 
-
-
-initLCD(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D1, LCD_D2, LCD_D3, LCD_D4); // New
+static LiquidCrystal lcd(LCD_RS_PIN, LCD_ENABLE_PIN, LCD_D1, LCD_D2, LCD_D3, LCD_D4); // New
 void doBacklight();
 void setColor(color_vars);
 void doDisplay();
@@ -30,4 +34,3 @@ void showCursor(boolean);
 
 
 #endif
-
